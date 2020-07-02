941e391c3dfbe5b0c71b96dce78af02a6973590c
Steven Lee <steven.lee1393@gmail.com>
2020-07-02 20:27:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   array.map do |element|
     element.upcase + "!"
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
